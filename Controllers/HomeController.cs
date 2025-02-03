a2c8eef3db82f6c05b056dc47c172ff8effe2520
Isaac Akinsanya <IsaacAki247@gmail.com>
2025-02-03 15:07:57 +0000

    Added page function to HomeController.cs
    - Created a readonly configuration to retrieve the connection string from appsettings.json
    
    - Created 'AddNewBook' method to display the page
    - Created 'NewBookConfirmation' method to display the page. The methos take the information passed from the AddNewBook page and calls the database functions AddBook to add it to the database and 'AddBookConfirmation' to display book information
    
    - Created 'DeleteBook'' method to display the page
    - Created 'DeleteBookConfirmation' method to display the page. The methos take the information passed from the DeleteBook page and calls the database functions DeleteBook to remove it from the database and 'AddBookConfirmation' to display book information

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace LocalLibrary.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public IActionResult Index()
@@ -18,9 +20,33 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult AddNewBook()
     {
         return View();
+    }
+
+    public IActionResult NewBookConfirmation(string name, string author, int copies)
+    {
+        Database.Instance.SetConnectionString(_configuration.GetValue<string>("ConnectionString"));
+        Database.Instance.AddBook(name, author, copies);
+        return View(Database.Instance.AddBookConfirmation(name, author));
+    }
+
+    public IActionResult DeleteBook()
+    {
+        return View();
+    }
+    public IActionResult DeleteBookConfirmation(string name, int isbn)
+    {
+        Database.Instance.SetConnectionString(_configuration.GetValue<string>("ConnectionString"));
+        Database.Instance.DeleteBook(name, isbn);
+        return View(Database.Instance.DeleteBookConfirmation(name, isbn));
+    }
+
+    public IActionResult AllBooks()
+    {
+        Database.Instance.SetConnectionString(_configuration.GetValue<string>("ConnectionString"));
+        return View(Database.Instance.GetAllBooks());
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
